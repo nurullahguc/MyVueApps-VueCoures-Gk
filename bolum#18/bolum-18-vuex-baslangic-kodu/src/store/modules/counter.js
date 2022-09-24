9b3bd1f52daa545e651fb26cf673d2f271b616a5
Niko JR <bbcn419a@gmail.com>
2022-09-24 21:58:40 +0300

    bolum#18 => Vuex İşlendi!

---
+++ bolum#18/bolum-18-vuex-baslangic-kodu/src/store/modules/counter.js
@@ -0,0 +1,42 @@
+const state = {
+    counter: 0,
+}
+
+const getters = {
+    getDoubleCounter(state) {
+        return state.counter * 2
+    },
+    stringCounter(state) {
+        return state.counter + ". kez tıklandı!"
+    },
+}
+
+const mutations = {
+    increaseCounter(state) {
+        state.counter++
+    },
+    decreaseCounter(state) {
+        state.counter--
+    },
+}
+
+const actions = {
+    increment({commit}) {
+        commit("increaseCounter")
+    },
+    decrement({commit}) {
+        commit("decreaseCounter")
+    },
+    incrementAsync({commit}, payload) {
+        setTimeout(() => {
+            commit('increaseCounter')
+        }, payload.time)
+    },
+}
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
