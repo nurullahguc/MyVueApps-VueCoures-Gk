d6d6430112f4673d775a6e1bc1e0c270c2ee41bf
Niko JR <bbcn419a@gmail.com>
2022-10-01 22:29:22 +0300

    urun islemleri uygulamasina yeniden baslandi

---
+++ bolum#19/19-urun-islemleri-uygulamasi-baslangic-kodu/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-header></app-header>
+     <router-view></router-view>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/shared/Header"
+import Footer from "./components/shared/Footer"
+import ProductList from "./components/products/ProductList"
+
+export default {
+  components: {
+    appHeader: Header,
+    appFooter: Footer,
+    appContainer: ProductList,
+  }
+}
+
+</script>
+
+<style>
+
+</style>
